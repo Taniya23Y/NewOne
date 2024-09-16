ac72b008beb32bd90f43b234679c0d12e9483244
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-09-16 16:12:23 +0530

    stater pack with nextjs

---
+++ portfolio/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-serif" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Taniya| Personal Portfolio",
+  description:
+    "Hi, I’m Taniya Yadav, a passionate Frontend Developer and MERN Stack Developer with a knack for crafting responsive, user-friendly web applications. I specialize in creating seamless digital experiences using modern technologies like React.js, Node.js, Express, and MongoDB, paired with Tailwind CSS for clean, efficient styling.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
